17adf150b45145daf3dfbaf9b88bc46c0684be37
Евгений Полуянович <eugeny.pol93@gmail.com>
2021-02-18 01:38:13 +0300

    Edited routing

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ app.use(express.json({ extended: true}))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/data', require('./routes/data.routes'))
-app.use(express.static(path.join(__dirname, 'public')))
-// if (process.env.NODE_ENV === 'production') {
-//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-//
-//     app.get('*', ((req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     }))
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', ((req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    }))
+}
 
 const PORT = process.env.PORT || 5000
 
